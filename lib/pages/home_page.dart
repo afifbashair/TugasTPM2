3d0890b8a45248d7c5f11eb5454623a21813b743
afifbashair <mafifbashair@gmail.com>
2026-03-10 22:12:53 +0700

    menambahkan data kelompok

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:berhitung_app/pages/kelompok_page.dart';
+import 'package:berhitung_app/pages/kalkulator_page.dart';
+import 'package:berhitung_app/pages/cekbilangan_page.dart';
+import 'package:berhitung_app/pages/jumlahbilangan_page.dart';
+import 'package:berhitung_app/pages/stopwatch_page.dart';
+import 'package:berhitung_app/pages/piramid_page.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Menu Utama")),
+      body: ListView(
+        children: [
+
+          ListTile(
+            title: Text("Data Kelompok"),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (_) => KelompokPage()));
+            },
+          ),
+
+          ListTile(
+            title: Text("Penjumlahan & Pengurangan"),
+            onTap: () {
+              // Navigator.push(context,
+              //     MaterialPageRoute(builder: (_) => KalkulatorPage()));
+            },
+          ),
+
+          ListTile(
+            title: Text("Ganjil / Genap & Bilangan Prima"),
+            onTap: () {
+              // Navigator.push(context,
+              //     MaterialPageRoute(builder: (_) => BilanganPage()));
+            },
+          ),
+
+          ListTile(
+            title: Text("Jumlah Total Angka"),
+            onTap: () {
+              // Navigator.push(context,
+              //     MaterialPageRoute(builder: (_) => JumlahAngkaPage()));
+            },
+          ),
+
+          ListTile(
+            title: Text("Stopwatch"),
+            onTap: () {
+              // Navigator.push(context,
+              //     MaterialPageRoute(builder: (_) => StopwatchPage()));
+            },
+          ),
+
+          ListTile(
+            title: Text("Luas & Volume Piramid"),
+            onTap: () {
+              // Navigator.push(context,
+              //     MaterialPageRoute(builder: (_) => PiramidPage()));
+            },
+          ),
+
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/kelompok_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class KelompokPage extends StatelessWidget {
+
+  final List<Map<String, String>> users = const [
+    {'username': 'Afif Maulana B', 'nim': '123210051'},
+    {'username': 'Muhammad Harsin', 'nim': '123200014'},
+    {'username': 'Syaidatul Munawwirah', 'nim': '123230036'},
+    {'username': 'Mada', 'nim': '123230000'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Data Kelompok")),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          final user = users[index];
+          return Card(
+            margin: const EdgeInsets.symmetric(vertical: 8),
+            child: ListTile(
+              leading: const CircleAvatar(
+                child: Icon(Icons.person),
+              ),
+              title: Text(user['username']!),
+              subtitle: Text('NIM: ${user['nim']}'),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
